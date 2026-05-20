1b9e4fead08c7c1f1ad097c07b1181cb07c0d11d
anjalideshmukh969 <anjalideshmukh2541@gmail.com>
2026-05-21 00:45:21 +0530

    setup database and folder structure

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+import express, { urlencoded } from "express";
+import dotenv from "dotenv";
+import connectDB from "./src/config/db.js";
+import cookieParser from "cookie-parser";
+
+
+const app = express();
+dotenv.config();
+app.use(express.json())
+app.use(cookieParser())
+
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+    connectDB();
+})
